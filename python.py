bd5af04d42a42d6d2952c9ea5a38ac7c22571eab
Shazrina97 <127166796+Shazrina97@users.noreply.github.com>
2023-03-07 14:33:29 +0800

    Update python.py

--- python.py
+++ python.py
@@ -23,3 +23,15 @@ st.write("Below are the data:")
 st.write(df)
 
 st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    tv_value = st.sidebar.slider('TV Value', 0, 150, 300)
+    radio_value = st.sidebar.slider('Radio Value', 0, 25, 50)
+    newspaper_value = st.sidebar.slider('Newspaper Value', 0, 75, 150)
+    data = {'TV Value': tv_value,
+            'Radio Value': radio_value,
+            'Newspaper Value': newspaper_value}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+uif = user_input_features()
