b1344b1c32ffc8dd02a4475e07c20e482bf317cf
Shazrina97 <127166796+Shazrina97@users.noreply.github.com>
2023-03-07 15:56:04 +0800

    Update python.py

--- python.py
+++ python.py
@@ -29,9 +29,9 @@ def user_input_features():
     tv_value = st.sidebar.slider('TV Value', 0, 150, 300)
     radio_value = st.sidebar.slider('Radio Value', 0, 25, 50)
     newspaper_value = st.sidebar.slider('Newspaper Value', 0, 75, 150)
-    data = {'TV Value': tv_value,
-            'Radio Value': radio_value,
-            'Newspaper Value': newspaper_value}
+    data = {'TV': tv_value,
+            'Radio': radio_value,
+            'Newspaper': newspaper_value}
     features = pd.DataFrame(data, index=[0])
     return features
 
