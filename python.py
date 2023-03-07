dcdb31a5a732a9ff38636c1250226d94067b054f
Shazrina97 <127166796+Shazrina97@users.noreply.github.com>
2023-03-07 14:43:27 +0800

    Update python.py

--- python.py
+++ python.py
@@ -39,7 +39,7 @@ uif = user_input_features()
 st.subheader('User Input parameters')
 st.write(uif)
 
-sales = df('Sales')
+sales = pd.read_csv("Advertising.csv", usecols = ['Sales'])
 X = sales.data
 Y = sales.target
 
