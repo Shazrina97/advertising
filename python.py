c509fe1d6b9e30ba7b55d06b6d2b7698a3165073
Shazrina97 <127166796+Shazrina97@users.noreply.github.com>
2023-03-07 14:29:58 +0800

    Update python.py

--- python.py
+++ python.py
@@ -18,40 +18,6 @@ st.write("""
 This app predicts the **Number of Sales**!
 """)
 
-
-st.sidebar.header('User Input Parameters')
-
-def user_input_features():
-    tv_value = st.sidebar.slider('TV Value', 0, 150, 300)
-    radio_value = st.sidebar.slider('Radio Value', 2.0, 4.4, 3.4)
-    newspaper_value = st.sidebar.slider('Newspaper Value', 1.0, 6.9, 1.3)
-    data = {'TV Value': tv_value,
-            'Radio Value': radio_value,
-            'Newspaper Value': newspaper_value}
-    features = pd.DataFrame(data, index=[0])
-    return features
-
-df = user_input_features()
-
-st.subheader('User Input parameters')
 st.write(df)
 
-sales = datasets.load_sales()
-X = sales.data
-Y = sales.target
 
-clf = RandomForestClassifier()
-clf.fit(X, Y)
-
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
-
-st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
-
-st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
